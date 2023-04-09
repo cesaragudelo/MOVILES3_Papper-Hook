41a1a26b67511e9e54dec3bd85bdd30c4fe85615
CESAR AGUDELO <cesaragudelo811@gmail.com>
2023-04-09 16:46:03 -0500

    formulario ok falta validacion

--- App.js
+++ App.js
@@ -46,10 +46,117 @@ export default function App() {
         )}
         name="fullname"
       />
+
       {errors.fullname?.type == 'required' && <Text style={{color:'red'}}>el nombre completo es obligatorio</Text>}
       {errors.fullname?.type == 'maxLength' && <Text style={{color:'red'}}>el nombre completo debe tener maximo 30 caracteres</Text>}
       {errors.fullname?.type == 'minLength' && <Text style={{color:'red'}}>el nombre completo debe tener minimo 2 caracteres</Text>}
       {errors.fullname?.type == 'pattern' && <Text style={{color:'red'}}>el nombre solo permite letras y espacios</Text>}
+
+
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+         maxLength:30,
+         minLength:6,
+        pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ@ ]+$/g,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Email"
+            mode='outlined'
+            left={<TextInput.Icon icon="email" />}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+
+      {errors.email?.type == 'required' && <Text style={{color:'red'}}>el correo completo es obligatorio</Text>}
+      {errors.email?.type == 'maxLength' && <Text style={{color:'red'}}>el correo completo debe tener maximo 30 caracteres</Text>}
+      {errors.email?.type == 'minLength' && <Text style={{color:'red'}}>el correo completo debe tener minimo 26 caracteres</Text>}
+      {errors.email?.type == 'pattern' && <Text style={{color:'red'}}>el correo solo permite letras y espacios</Text>} 
+      
+
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+         maxLength:30,
+         minLength:2,
+         pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Phone"
+            mode='outlined'
+            left={<TextInput.Icon icon="phone" />}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="phone"
+      />
+
+      {errors.phone?.type == 'required' && <Text style={{color:'red'}}>el telefono completo es obligatorio</Text>}
+      {errors.phone?.type == 'maxLength' && <Text style={{color:'red'}}>el telefono completo debe tener maximo 10 numeros</Text>}
+      {errors.phone?.type == 'minLength' && <Text style={{color:'red'}}>el telefono completo debe tener minimo 6 numeros</Text>}
+      {errors.phone?.type == 'pattern' && <Text style={{color:'red'}}>el telefono solo permite numeros</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+         maxLength:30,
+         minLength:5,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Password"
+            secureTextEntry
+            mode='outlined'
+            left={<TextInput.Icon icon="key" />}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+
+      {errors.password?.type == 'required' && <Text style={{color:'red'}}>el password es obligatorio</Text>}
+      {errors.password?.type == 'maxLength' && <Text style={{color:'red'}}>el password completo debe tener maximo 30 caracteres</Text>}
+      {errors.password?.type == 'minLength' && <Text style={{color:'red'}}>el pasword completo debe tener minimo 5 numeros</Text>}
+      
+
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+         maxLength:3,
+         minLength:1,
+         pattern:/^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/g+1-9
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Age"
+            mode='outlined'
+            left={<TextInput.Icon icon="calendar" />}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="age"
+      />
+
+      {errors.age?.type == 'required' && <Text style={{color:'red'}}>la edad es obligatoria</Text>}
+      {errors.age?.type == 'maxLength' && <Text style={{color:'red'}}>la edad debe tener maximo 3 numeros</Text>}
+      {errors.age?.type == 'minLength' && <Text style={{color:'red'}}>la edad debe tener minimo 1 numero</Text>}
+      {errors.age?.type == 'pattern' && <Text style={{color:'red'}}>la edad solo permite numeros</Text>}
 
         <Button 
         style={{marginTop:20}}
